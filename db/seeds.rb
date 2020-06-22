939309aec8d300ec37a7a9c05cd74b89c13b720c
thierry vinagre <vinagre.thierry@yahoo.fr>
2020-06-22 13:52:14 +0200

    Adding 2 motels in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating motels...'
+Motel.create!({
+ name: "Georges V",
+ address: "Place Vendom, 75004 Paris, France"
+})
+Motel.create!({
+ name: "Magistic",
+ address: "Promenade des anglais 06007 Cannes, France"
+})
+puts 'Finished!'
+
+
