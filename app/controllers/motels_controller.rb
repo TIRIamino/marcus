6358da5106af56c235cfc89671671f31a6b0665a
thierry vinagre <vinagre.thierry@yahoo.fr>
2020-06-22 14:19:08 +0200

    Adding views/index&show  motel

---
+++ app/controllers/motels_controller.rb
@@ -0,0 +1,9 @@
+class MotelsController < ApplicationController
+ def index
+  @motels = Motel.all
+ end
+
+ def show
+  @motel = Motel.find(params[:id])
+ end
+end
---
+++ test/controllers/motels_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MotelsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
